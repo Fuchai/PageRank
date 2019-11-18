a127e9a6fa09e93e9038d23fe4fcb57b978c95ca
sjs5904 <songjooseung931026@gmail.com>
2019-11-17 23:14:41 -0600

    SpamFarm+PreProcessing

--- src/PreProcessing.java
+++ src/PreProcessing.java
@@ -1,38 +1,52 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class PreProcessing {
+	
+	public static void main(String[] args) {
+		PreProcessing pr = new PreProcessing();
+		File tempFile = new File("./data/testPre.txt");
+        String[] arr = pr.process(tempFile);
+        for (int i = 0; i < arr.length; i++) {
+        	System.out.println(arr[i] + i);
+        }
+    } 
+	
+	public PreProcessing() {
+		
+	}
 
     // from the last project
-    //    public static String[] process(File file) {
-//
-//        Scanner s;
-//        try {
-//            s = new Scanner(file);
-//            ArrayList<String> list = new ArrayList<>();
-//
-//            while (s.hasNext()) {
-//                String word = s.next();
-//                word = word.toLowerCase();
-//                word = word.replaceAll("\\.|,|;|:|'", "");
-//                if (word.length() > 2 && !word.equals("the")) {
-//                    list.add(word);
-//                }
-//            }
-//            String[] ret = new String[list.size()];
-//            ret = list.toArray(ret);
-//            return ret;
-//        } catch (FileNotFoundException e) {
-//            System.out.println("File not found: " + file.getAbsolutePath());
-//            e.printStackTrace();
-//            System.exit(-10);
-//        }
-//        // will not reach
-//        return null;
-//    }
+	public static String[] process(File file) {
+
+		Scanner s;
+        try {
+            s = new Scanner(file);
+            ArrayList<String> list = new ArrayList<>();
+
+            while (s.hasNext()) {
+                String word = s.next();
+                word = word.toLowerCase();
+                word = word.replaceAll("[\\,;:?'(){}\\[\\]\"]", "");
+                word = word.replaceAll("([^0-9])\\.([^0-9])", "$1$2");
+                
+                list.add(word);
+            }
+            String[] ret = new String[list.size()];
+            ret = list.toArray(ret);
+            return ret;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + file.getAbsolutePath());
+            e.printStackTrace();
+            System.exit(-10);
+        }
+        // will not reach
+        return null;
+    }
 
 
 }
